7de0f46e957b2d6ff7a2557736a58b8027e7863e
TANMOY PRAMANICK <pramanickt15@gmail.com>
2024-06-21 00:41:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">sourav with archu😍 Jaldi baha heijao🥰 sure heijiba.</p>
+      <p className="read-the-docs">sourav with Archana😍 Jaldi baha heijao🥰 sure heijiba.</p>
     </>
   );
 }
